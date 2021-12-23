7f5fdf56315cefe144b490d9a0233e56f9d14914
paulino-s <paulinovasquez900@gmail.com>
2021-12-23 14:02:14 -0600

    add env-prod and env-pre

--- dbfiles/init-user-db.sh
+++ dbfiles/init-user-db.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER billingapp WITH PASSWORD '';
+    CREATE USER billingapp WITH PASSWORD 'qwerty';
     CREATE DATABASE billingapp_db;
     GRANT ALL PRIVILEGES ON DATABASE billingapp_db TO billingapp;
 EOSQL
